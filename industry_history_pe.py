ed1ee07d73c8df6dff6e929eb1e6f4ec4090e95e
Clarence.Zhou <zhou_chenz@163.com>
2020-03-16 23:57:06 +0800

    extract the common code and add muti-curve comparison

--- industry_history_pe.py
+++ industry_history_pe.py
@@ -1,67 +1,47 @@
 #!/usr/bin/env python3
 # coding: utf-8
 
-import numpy as np
-import pandas as pd
-from visdom import Visdom
-import matplotlib as mpl
-import matplotlib.pyplot as plt
-import matplotlib.dates as mdate
-import seaborn
-seaborn.set()
-# import pylab import mpl
-import os, time, sys, pickle
-from dateutil.parser import parse
-
-deprecated_date_list = ['2016-08-15']
-font = {'family': 'SimHei'}
-
-def filter_exception_date_in_series(data_series):
-    # drop deprecated date index
-    for date in deprecated_date_list:
-        data_series = data_series.drop(parse(date))
-    return data_series
+from industry_pe_common import *
 
 if __name__=='__main__':
     if len(sys.argv) < 2:
-        print("pls enter dataset file path!")
+        print("please enter Industry code index file path")
         raise SystemExit(1)
-
-    csv_data_file = sys.argv[1]
-    data_frame = pd.read_csv(csv_data_file)
-    date_list = data_frame['date'].apply(str).apply(parse)
-    name = data_frame['industry_name'][0]
-    industry_code = data_frame['industry_code'][0]
-    history_count = np.array(data_frame['company_count'])
-    static_pe_average = np.array(data_frame['static_pe_average'])
-    static_pe_median = np.array(data_frame['static_pe_median'])
-    dynamic_pe_average = np.array(data_frame['dynamic_pe_average'])
-    dynamic_pe_median = np.array(data_frame['dynamic_pe_median'])
-    print(name)
-    print(data_frame.index)
-    print(data_frame.columns)
-    dpa_data_series = pd.Series(dynamic_pe_average, index=date_list.values).sort_index(ascending=False)
-    # print('2015-12-25: ', dpa_data_series['2015-12-25'])
-    # print('2016-08-15: ', dpa_data_series['2016-08-15'])
-    dpa_data_series = filter_exception_date_in_series(data_series=dpa_data_series)
-    print(dpa_data_series)
-    # print(dpa_data_series[parse('2020-03-06')])
-    print(dpa_data_series[parse('2015-12-21')])
+    global default_dateset_path
+    industry_code_file_f = open(sys.argv[1], 'r+', encoding='utf-8-sig')
+    industry_code_dict= json.load(industry_code_file_f)
+    industry_code_file_f.close()
+    # print(industry_code_dict)
+    for item in industry_code_dict:
+        print(item, ': ', industry_code_dict[item])
+    print('请从以上代码中选择要显示的行业代码列表, 用空格键分隔, 忽略大小写: ')
+    input_code_list = input().upper().split(' ')
+    print(input_code_list)
+    print(default_dateset_path)
+    max_pe = 0.0
+    for code in input_code_list:
+        csv_data_file = default_dateset_path + code + '.csv'
+        data_frame = pd.read_csv(csv_data_file)
+        date_list = data_frame['date'].apply(str).apply(parse)
+        name = data_frame['industry_name'][0]
+        industry_code = data_frame['industry_code'][0]
+        history_count = np.array(data_frame['company_count'])
+        static_pe_average = np.array(data_frame['static_pe_average'])
+        static_pe_median = np.array(data_frame['static_pe_median'])
+        dynamic_pe_average = np.array(data_frame['dynamic_pe_average'])
+        dynamic_pe_median = np.array(data_frame['dynamic_pe_median'])
+        print(name)
+        print(data_frame.index)
+        print(data_frame.columns)
+        dpa_data_series = pd.Series(dynamic_pe_average, index=date_list.values).sort_index(ascending=False)
+        dpa_data_series = filter_exception_date_in_series(data_series=dpa_data_series)
+        tmp_max_val = dpa_data_series.values.max()
+        print('tmp_max_val: ', tmp_max_val)
+        if max_pe < tmp_max_val:
+            max_pe = tmp_max_val
+        print(dpa_data_series)
+        # print(dpa_data_series[parse('2015-12-21')])
     # pe_list = dpa_data_series.tolist()
     # dpa_data_series.plot()
-    plt.style.use('seaborn-whitegrid')
-    plt.plot(dpa_data_series.index, dpa_data_series.values, color='r', label=industry_code + name)
-    plt.ylabel(u'市盈率', fontproperties='SimHei')
-    plt.xlabel(u'时间轴', fontproperties='SimHei')
-    plt.title(u'动态市盈率均值', fontproperties='SimHei')
-    plt.ylim(-1, 150)
-    plt.legend(loc=0, prop=font)
-    plt.grid(True)
-   # plt.hist(dpa_data_series)
-   # plt.title('industry history pe')
-   # plt.xlabel('date')
-   # plt.ylabel('pe')
-    viz = Visdom(env='main')
-    viz.matplot(plt)
-
-
+        plt.plot(dpa_data_series.index, dpa_data_series.values, label=industry_code + name)
+    show_pe_chart(max_pe=max_pe)
---
+++ industry_pe_common.py
@@ -0,0 +1,42 @@
+#!/usr/bin/env python3
+# coding: utf-8
+
+import json
+import numpy as np
+import pandas as pd
+from visdom import Visdom
+import matplotlib as mpl
+import matplotlib.pyplot as plt
+import matplotlib.dates as mdate
+import seaborn
+seaborn.set()
+# import pylab import mpl
+import os, time, sys, pickle
+from dateutil.parser import parse
+
+deprecated_date_list = ['2016-08-15']
+font = {'family': 'SimHei'}
+default_dateset_path = './stock_scrapy/data_collect/industry_pe_history/'
+titles = {'family': 'SimHei'}
+
+def filter_exception_date_in_series(data_series):
+    # drop deprecated date index
+    for date in deprecated_date_list:
+        data_series = data_series.drop(parse(date))
+
+    data_series = data_series.dropna()
+    return data_series
+
+def show_pe_chart(max_pe=150):
+    plt.style.use('seaborn-whitegrid')
+    plt.ylabel(u'市盈率', fontproperties='SimHei')
+    plt.xlabel(u'时间轴', fontproperties='SimHei')
+    plt.title(u'动态市盈率均值', fontproperties='SimHei')
+    plt.ylim(-1, max_pe+10)
+    plt.legend(loc=0, prop=font)
+    plt.grid(True)
+    viz = Visdom(env='main')
+    viz.matplot(plt)
+
+
+
